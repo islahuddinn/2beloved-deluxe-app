34fd2bbd4d07d4e01cd7cc37485a268ac8824c20
humaisawan <humaisawan0@gmail.com>
2024-05-29 14:12:20 +0500

    testing post boost expiry

--- utils/CronJob.js
+++ utils/CronJob.js
@@ -1,35 +1,27 @@
 const cron = require("node-cron");
 const moment = require("moment");
-const User = require('../models/userModel');
-
+const User = require("../models/userModel");
 
 const checkBoostExpiry = async () => {
   try {
-    const currentDate = new Date();
-    
     const users = await User.find({
       "boost.isBoostActive": true,
-      "boost.boostExpireDate": { $exists: true, $lte: currentDate }
     });
-
+    console.log("USERS WITH BOOST ACTIVE ARE:", users);
     if (users.length > 0) {
       
-      const bulkOps = users.map(user => ({
-        updateOne: {
-          filter: { _id: user._id },
-          update: {
-            $set: {
-              "boost.isBoostActive": false,
-              "boost.boostStartDate": undefined,
-              "boost.boostExpireDate": undefined
-            }
-          }
-        }
-      }));
+      for (const user of users) {
+        const currentDate = new Date();
+        if (currentDate > user.boost.boostExpireDate) {
+          user.boost.isBoostActive = false;
+          user.boost.boostStartDate = undefined;
+          user.boost.boostExpireDate = undefined;
 
-      await User.bulkWrite(bulkOps);
+          await user.save();
+        }
+      }
     }else{
-      console.log("NO USER FOUND WITH BOOST ACTIVE AND EXPIRY LESS THAN CURRENT DATE")
+      console.log("NO USER FOUND WITH ACTIVE BOOST STATUS")
     }
   } catch (error) {
     console.log("ERROR WHILE CHECKING BOOST EXPIRY CRON JOB:", error);
@@ -39,6 +31,5 @@ module.exports = () => {
   cron.schedule("* * * * *", async () => {
     console.log("CRON JOB STARTED!!!");
     await checkBoostExpiry();
-    
   });
 };
