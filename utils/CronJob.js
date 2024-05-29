b294f0439301f47276f42f230368de0ea5f3aba0
humaisawan <humaisawan0@gmail.com>
2024-05-29 14:29:08 +0500

    testing boost expiry

--- utils/CronJob.js
+++ utils/CronJob.js
@@ -3,30 +3,16 @@ const moment = require("moment");
 const User = require("../models/userModel");
 
 const checkBoostExpiry = async () => {
-  try {
+  
     const users = await User.find();
-    console.log("USERS WITH BOOST ACTIVE ARE:", users);
-    if (users.length > 0) {
-      for (const user of users) {
-        if (user.boost.isBoostActive === "true") {
-          const currentDate = new Date();
-          if (currentDate > user.boost.boostExpireDate) {
-            user.boost.isBoostActive = false;
-            user.boost.boostStartDate = undefined;
-            user.boost.boostExpireDate = undefined;
+    const allUsersLength = users.length
+    console.table([users, allUsersLength])
+    const boostedUsers = users.filter((user)=> user.boost.isBoostActive === 'true')
+   const boostedUsersLength = boostedUsers.length
 
-            await user.save();
-          }else{
-            console.log("ACTIVE STATUS IS NOT EXPIRED YET.")
-          }
-        }
-      }
-    } else {
-      console.log("NO USER FOUND WITH ACTIVE BOOST STATUS");
-    }
-  } catch (error) {
-    console.log("ERROR WHILE CHECKING BOOST EXPIRY CRON JOB:", error);
-  }
+   console.table([boostedUsers, boostedUsersLength])
+    
+   
 };
 module.exports = () => {
   cron.schedule("* * * * *", async () => {
